6c151be951473c94ccc10ce78e405399614e7ec9
Perlar118 <perlamaria9545@gmail.com>
2023-11-15 00:54:31 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Ejericicospracti4.clases;
+using System;
+
+namespace Ejericicospracti4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Ejercicios ejercicios = new Ejercicios();
+            ejercicios.Arreglo();
+        }
+    }
+}
